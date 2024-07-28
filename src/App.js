42d0a668f9bf59f0d5303b4f48cd5c78a42b2095
Kyu hyunSung <dmp100@naver.com>
2024-07-28 17:39:06 +0900

    Temporary commit to switch branches

--- src/App.js
+++ src/App.js
@@ -1,63 +1,24 @@
-// App.js
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreateSpeechPage from './screen/CreateSpeechPage';
-import GeneratedSpeechPage from './screen/GeneratedSpeechPage';
-import PracticeSpeechPage from './screen/PracticeSpeechPage';
-import MainPage from './screen/MainPage';
-import LoginPage from './screen/LoginPage';
-import RegisterPage from './screen/RegisterPage';
-import PasswordResetPage from './screen/PasswordResetPage';
-import GenerationPage from './screen/GenerationPage';
-import HelpWithSpeechPage from './screen/HelpWithSpeechPage';
-import ViewMemoriesPage from './screen/ViewMemoriesPage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [speechData, setSpeechData] = useState({});
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-
-  const showModal = (data) => {
-    setSpeechData(data);
-    setModalVisible(true);
-  };
-
-  const hideModal = () => {
-    setModalVisible(false);
-  };
-
-  const login = (userData) => {
-    setIsLoggedIn(true);
-    setUser(userData);
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-    setUser(null);
-  };
-
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<MainPage isLoggedIn={isLoggedIn} user={user} logout={logout} />} />
-          <Route path="/create" element={<CreateSpeechPage showModal={showModal} />} />
-          <Route path="/practice" element={<PracticeSpeechPage />} />
-          <Route path="/login" element={<LoginPage login={login} />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/forgot-password" element={<PasswordResetPage />} />
-          <Route path="/generation" element={<GenerationPage />} />
-          <Route path="/help-with-speech" element={<HelpWithSpeechPage />} />
-          <Route path="/view-memories" element={<ViewMemoriesPage />} />
-        </Routes>
-        {modalVisible && (
-          <div className="modal-backdrop">
-            <GeneratedSpeechPage speechData={speechData} hideModal={hideModal} />
-          </div>
-        )}
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
